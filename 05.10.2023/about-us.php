1c191b63d74ff790ba5918dcfee39b9abb67f00c
Michał Wieruszewski <145012937+MichalWZSK@users.noreply.github.com>
2023-10-05 13:50:20 +0200

    Add files via upload

---
+++ 05.10.2023/about-us.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>\
+    <style>
+        body {font-family: 'Helvetica', sans-serif;
+    background-color: linen;
+    color:#222;
+    margin: auto;
+    width: 90%;}
+header {background-color: black;
+        color: white;
+        display: flex;
+        height: 100%;}
+div, nav {width: 50%;}
+section {padding: 20px;}
+footer {background-color: black;
+        color: white;
+        padding: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;}
+
+table, td{border: 1px solid black;
+            border-collapse: collapse;
+        }
+table {margin: 40px;}
+td {padding: 30px;
+    text-align: center;
+    font-weight: bold;
+    }
+td {padding: 30px;
+    text-align: center;
+    font-weight: bold;}
+a  {color: #aaa;
+    text-decoration: none;
+    border-bottom: 1px solid white;}
+a:hover {color: white;}
+    </style>
+</head>
+<body>
+<h1><?php echo "O nas"; ?></h1>
+<p>Cokolwiek</p>
+<p>Cokolwiek1</p>
+    <nav>
+        <ol>
+            <li>1</li>    
+            <li>1</li>
+            <li>1</li>
+        </ol>
+
+    <footer>
+        <p>Copyright &copy 2023</p>
+    </footer>
+
+ 
+</body>
+</html>
