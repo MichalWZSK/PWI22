9237525463c13c1944886e754e6c63b125ad8f27
Michał Wieruszewski <145012937+MichalWZSK@users.noreply.github.com>
2024-01-25 13:55:42 +0100

    Add files via upload

---
+++ 25.01.2024-not ready/kalendarz.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Kalendarz</title>
+</head>
+<body>
+    <div class="gorny_lewy">
+        <img src="logo1.png" alt="Kalendarz1">
+    </div>
+    <div class="gora_praw">
+        <h1>KALENDARZ</h1>
+        <p>miesiąc: lipiec, rok:2020</p>
+    </div>
+    
+</body>
+</html>
